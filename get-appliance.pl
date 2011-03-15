1bcff7c3d6e63e04a7b9d72e15a36f79e91d5b81
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-15 13:49:33 -0700

    simple Perl script to grab an appliance

---
+++ get-appliance.pl
@@ -0,0 +1,10 @@
+#! /usr/bin/perl -w
+my ($link) = @ARGV;
+print "Fetching ${link}\n";
+my @fields = split /\//, $link;
+my $md5sum = "${fields[4]}  ${fields[5]}\n";
+system "echo \'${md5sum}\' | head -n 1 > md5";
+system "wget ${link}";
+system "md5sum -c md5";
+system "tar xf ${fields[5]}";
+exit;
