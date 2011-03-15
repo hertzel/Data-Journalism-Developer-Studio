e80129b5bdae8422da0f7b29df8ce964009f7033
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-14 21:01:17 -0700

    need libxml2-devel now

--- Desktop/Install-Scripts/install-dependencies.bash
+++ Desktop/Install-Scripts/install-dependencies.bash
@@ -27,8 +27,7 @@ sudo zypper --non-interactive in fftw3 fftw3-devel fftw3-threads \
   fftw3-threads-devel freeglut-devel libpng14-devel
 
 # needed for LaTeX / SWeave
-sudo zypper --non-interactive in texlive-bin-latex texlive-bin
-#libxml2-devel
+sudo zypper --non-interactive in texlive-bin-latex texlive-bin libxml2-devel
 
 # R, Chromium and nokogiri
 sudo zypper addrepo -cf \
