acfcb5e044c722c7c2cc9d3d39abd0b4b4bfc66c
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-16 21:42:12 -0700

    unpack only if file is compressed

--- get-appliance.pl
+++ get-appliance.pl
@@ -6,5 +6,5 @@ my $md5sum = "${fields[4]}  ${fields[5]}\n";
 system "echo \'${md5sum}\' | head -n 1 > md5";
 system "wget ${link}";
 system "md5sum -c md5";
-system "tar xf ${fields[5]}";
+system "tar xf ${fields[5]}" if ${fields[5]} =~ /\.gz$/;
 exit;
