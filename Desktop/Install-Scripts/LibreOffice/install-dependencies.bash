7599258db84ac732e2aa39e22d36920439dc5c2f
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-18 10:25:47 -0700

    added LibreOffice installer

---
+++ Desktop/Install-Scripts/LibreOffice/install-dependencies.bash
@@ -0,0 +1,30 @@
+#! /bin/bash -v
+
+sudo zypper install -y libreoffice
+sudo zypper install -y libreoffice-base
+sudo zypper install -y libreoffice-base-drivers-postgresql
+sudo zypper install -y libreoffice-base-extensions
+sudo zypper install -y libreoffice-branding-openSUSE
+sudo zypper install -y libreoffice-calc
+sudo zypper install -y libreoffice-calc-extensions
+sudo zypper install -y libreoffice-converter
+sudo zypper install -y libreoffice-draw
+sudo zypper install -y libreoffice-draw-extensions
+sudo zypper install -y libreoffice-filters
+sudo zypper install -y libreoffice-filters-optional
+sudo zypper install -y libreoffice-gnome
+sudo zypper install -y libreoffice-help-en-US
+sudo zypper install -y libreoffice-hyphen
+sudo zypper install -y libreoffice-impress
+sudo zypper install -y libreoffice-impress-extensions
+sudo zypper install -y libreoffice-languagetool-en
+sudo zypper install -y libreoffice-mailmerge
+sudo zypper install -y libreoffice-math
+sudo zypper install -y libreoffice-openclipart
+sudo zypper install -y libreoffice-templates-en
+sudo zypper install -y libreoffice-templates-labels-a4
+sudo zypper install -y libreoffice-templates-labels-letter
+sudo zypper install -y libreoffice-templates-presentation-layouts
+sudo zypper install -y libreoffice-thesaurus-en-US
+sudo zypper install -y libreoffice-writer
+sudo zypper install -y libreoffice-writer-extensions
