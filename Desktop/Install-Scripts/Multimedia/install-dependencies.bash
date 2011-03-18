e8af86cb2d0ca1bbc6184e2cb228044a6c1d8bec
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-18 13:08:16 -0700

    Multimedia creation packages

---
+++ Desktop/Install-Scripts/Multimedia/install-dependencies.bash
@@ -0,0 +1,31 @@
+#! /bin/bash -v
+
+# base pattern
+sudo zypper install -y -t pattern multimedia
+
+# audio
+sudo zypper install -y audacity
+
+# video and animation
+sudo zypper install -y blender blender-doc
+sudo zypper install -y pitivi
+sudo zypper install -y gtk-recordMyDesktop
+
+# document creation
+sudo zypper install -y scribus
+sudo zypper install -y inkscape
+sudo zypper install -y inkscape-extensions-dia
+sudo zypper install -y inkscape-extensions-extra
+sudo zypper install -y inkscape-extensions-fig
+sudo zypper install -y inkscape-extensions-gimp
+sudo zypper install -y inkscape-extensions-skencil
+sudo zypper install -y gimp
+sudo zypper install -y gimp-branding-openSUSE
+sudo zypper install -y gimp-gap
+sudo zypper install -y gimp-help
+sudo zypper install -y gimp-module-hal
+sudo zypper install -y gimp-plugin-lqr
+sudo zypper install -y gimp-plugins-python
+sudo zypper install -y gimp-save-for-web
+sudo zypper install -y gimp-ufraw
+sudo zypper install -y gutenprint-gimpplugin
