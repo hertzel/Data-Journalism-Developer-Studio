4c9bce394ebc530ca2b7df4569de6f811169d576
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-19 10:59:47 -0700

    moved install script cleanup where it belongs

--- Desktop/Install-Scripts/cleanup.bash
+++ Desktop/Install-Scripts/cleanup.bash
@@ -1,4 +1,6 @@
 #! /bin/bash -v
+sudo rm -fr rstudio
+sudo rm -fr build
 rm *.log
 for i in BrowserDevelopment/ Finance/ LibreOffice/ Multimedia/ \
   NaturalLanguageProcessing/ Perl-packages/ Python-NLTK/ ReproducibleResearch/ \
--- make-Desktop-tarball.bash
+++ make-Desktop-tarball.bash
@@ -6,10 +6,7 @@ pushd Desktop
 rm -fr google-refine-2.0
 tar xf ../google-refine-2.0-r1836.tar.gz
 popd
-pushd Desktop/Install-Scripts/Perl-packages
+pushd Desktop/Install-Scripts/
 ./cleanup.bash
 popd
-sudo rm -fr Desktop/Install-Scripts/build
-sudo rm -fr Desktop/Install-Scripts/rstudio
-sudo rm -fr Desktop/Install-Scripts/*.log
 tar cvf - Desktop | bzip2 -9c > Desktop.tar.bz2
