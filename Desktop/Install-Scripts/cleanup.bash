ff1aa23a21c5f93d22be86d44831262bb821d1e8
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-20 10:49:02 -0700

    new directories to clean

--- Desktop/Install-Scripts/cleanup.bash
+++ Desktop/Install-Scripts/cleanup.bash
@@ -4,7 +4,7 @@ sudo rm -fr build
 rm *.log
 for i in BrowserDevelopment/ Finance/ LibreOffice/ Multimedia/ \
   NaturalLanguageProcessing/ Perl-packages/ Python-NLTK/ ReproducibleResearch/ \
-  Spatial/ Sweave-LyX/
+  Spatial/ Sweave-LyX/ GGobi/ JGR/ Mallet/ Mondrian/ OpenFTS/ Rattle/ Rcmdr/
 do
   pushd ${i}
   ./cleanup.bash
