14de03e754641903800fe3049850ba48b83d4a4b
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-20 10:49:45 -0700

    changed to lower case "jgr"

--- Desktop/Install-Scripts/JGR/install-all.bash
+++ Desktop/Install-Scripts/JGR/install-all.bash
@@ -9,4 +9,4 @@ do
   sudo ./install-${i}.bash 2>&1 | tee ${i}.log
 done
 
-sudo ln -sf /usr/lib64/R/library/JGR/scripts/run /usr/local/bin/JGR
+sudo ln -sf /usr/lib64/R/library/JGR/scripts/run /usr/local/bin/jgr
