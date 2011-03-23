283e7378d253d3dd3519e5b4e70a1d230d8d0c97
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-22 21:10:58 -0700

    Added Jonathan Lees' Computational Seismology packages

---
+++ Desktop/Install-Scripts/Seismic/load-task-view.R
@@ -0,0 +1,11 @@
+update.packages(ask=FALSE, repos='http://cran.cnr.berkeley.edu')
+install.packages(c(
+  'RSEIS',
+  'GEOmap',
+  'RFOC',
+  'RPMG',
+  'Rwave',
+  'RTOMO',
+  'WINRPACK'), 
+  repos='http://cran.cnr.berkeley.edu'
+)
