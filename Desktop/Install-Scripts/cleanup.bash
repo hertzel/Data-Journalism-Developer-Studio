772757f2aadca95b72777ee9f2ab0fbb3b0d47a9
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-24 23:54:42 -0700

    added more directories to cleanup.bash

--- Desktop/Install-Scripts/cleanup.bash
+++ Desktop/Install-Scripts/cleanup.bash
@@ -2,9 +2,27 @@
 sudo rm -fr rstudio
 sudo rm -fr build
 rm *.log
-for i in BrowserDevelopment/ Finance/ LibreOffice/ Multimedia/ \
-  NaturalLanguageProcessing/ Perl-packages/ Python-NLTK/ ReproducibleResearch/ \
-  Spatial/ Sweave-LyX/ GGobi/ JGR/ Mallet/ Mondrian/ OpenFTS/ Rattle/ Rcmdr/
+for i in \
+  BrowserDevelopment/ \
+  ComputationalSeismology/ \
+  Finance/ \
+  GGobi/ \
+  JGR/ \
+  LibreOffice/ \
+  Mallet/ \
+  Mondrian/ \
+  Multimedia/ \
+  NaturalLanguageProcessing/ \
+  OpenFTS/ \
+  Perl-packages/ \
+  Python-NLTK/ \
+  Rattle/ \
+  Rcmdr/ \
+  ReproducibleResearch/ \
+  RPy2/ \
+  RSRuby/ \
+  Spatial/ \
+  Sweave-LyX/
 do
   pushd ${i}
   ./cleanup.bash
