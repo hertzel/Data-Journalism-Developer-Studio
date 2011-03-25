b677a29f5774ce1d4552dd7069d6b7cb91ffb8e9
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-24 23:56:01 -0700

    added more directories to install-options.bash

--- Desktop/Install-Scripts/install-options.bash
+++ Desktop/Install-Scripts/install-options.bash
@@ -1,7 +1,25 @@
 #! /bin/bash -v
-for i in BrowserDevelopment/ Finance/ GGobi/ JGR/ LibreOffice/ Mallet/ \
-  Mondrian/ Multimedia/ NaturalLanguageProcessing/ OpenFTS/ Python-NLTK/ \
-  Rattle/ Rcmdr/ ReproducibleResearch/ Spatial/ Sweave-LyX/
+for i in \
+  BrowserDevelopment/ \
+  ComputationalSeismology/ \
+  Finance/ \
+  GGobi/ \
+  JGR/ \
+  LibreOffice/ \
+  Mallet/ \
+  Mondrian/ \
+  Multimedia/ \
+  NaturalLanguageProcessing/ \
+  OpenFTS/ \
+  Perl-packages/ \
+  Python-NLTK/ \
+  Rattle/ \
+  Rcmdr/ \
+  ReproducibleResearch/ \
+  RPy2/ \
+  RSRuby/ \
+  Spatial/ \
+  Sweave-LyX/
 do
   pushd ${i}
   ./cleanup.bash
