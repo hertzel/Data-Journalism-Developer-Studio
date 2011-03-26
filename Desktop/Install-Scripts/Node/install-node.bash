60972b883bda695f5ccd51ff57afd5ab6e6e99aa
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-26 11:14:57 -0700

    Node.js

---
+++ Desktop/Install-Scripts/Node/install-node.bash
@@ -0,0 +1,22 @@
+#! /bin/bash -v
+
+mkdir -p /usr/local/src
+pushd /usr/local/src
+export DIR=node-v0.4.4
+export WHAT=${DIR}.tar.gz
+export WHERE=http://nodejs.org/dist
+rm -fr ${WHAT} ${DIR}
+wget ${WHERE}/${WHAT}
+tar xf ${WHAT}
+
+pushd ${DIR}
+./configure
+make 
+make install
+/sbin/ldconfig
+/sbin/SuSEconfig
+popd
+
+# clean up
+rm -fr ${WHAT} ${DIR}
+popd
