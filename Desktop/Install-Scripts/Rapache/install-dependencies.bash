e3daa540aab26a09563b18fb4b73cd99cff8289f
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-28 01:13:09 -0700

    non-interactive

--- Desktop/Install-Scripts/Rapache/install-dependencies.bash
+++ Desktop/Install-Scripts/Rapache/install-dependencies.bash
@@ -1,4 +1,4 @@
 #! /bin/bash -v
 
-sudo zypper install apache2 apache2-devel apache2-doc apache2-prefork \
+sudo zypper install -y apache2 apache2-devel apache2-doc apache2-prefork \
   apache2-utils yast2-http-server
