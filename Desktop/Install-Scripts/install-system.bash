5192b80ef0c568b563365333de1aad4d99ae35f1
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-27 21:06:51 -0700

    added edit step for the Renviron variable setter

--- Desktop/Install-Scripts/install-system.bash
+++ Desktop/Install-Scripts/install-system.bash
@@ -3,3 +3,13 @@
 export PATH=/usr/local/bin:$PATH
 unset JAVA_HOME; R CMD javareconf # needed for rJava
 R --vanilla --slave < load-system.R 2>&1 | tee load-system.log
+
+# set up environment variables, e.g., PDF reader
+if [ -e /usr/lib/R/etc/Renviron ]
+then
+  gvim /usr/lib/R/etc/Renviron
+fi
+if [ -e /usr/lib64/R/etc/Renviron ]
+then
+  gvim /usr/lib64/R/etc/Renviron
+fi
