932fe484605529e7463e5d46cc3029fd7847b672
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-30 21:09:19 -0700

    added scripts to install R-patched from source

---
+++ Desktop/Install-Scripts/R-patched/install-R.bash
@@ -0,0 +1,52 @@
+#! /bin/bash -v
+
+# first, get rid of installed R!
+zypper remove R-patched R-patched-devel
+if [ -e /usr/lib/R ]
+then
+  rm -fr /usr/lib/R
+fi
+if [ -e /usr/lib64/R ]
+then
+  rm -fr /usr/lib64/R
+fi
+
+mkdir -p /usr/local/src
+cd /usr/local/src
+export WHERE=ftp://ftp.stat.math.ethz.ch/Software/R
+export DIR=R-patched
+export WHAT=R-patched.tar.gz
+rm -fr ${WHAT} ${DIR}
+wget ${WHERE}/${WHAT}
+tar xf ${WHAT}
+
+cd ${DIR}
+export R_PAPERSIZE='letter'
+export R_BROWSER='chromium'
+export R_PDFVIEWER='evince'
+./configure \
+  --enable-threads \
+  --enable-static \
+  --enable-R-profiling \
+  --enable-BLAS-shlib \
+  --enable-R-shlib \
+  --enable-R-static-lib \
+  --with-tcltk \
+  --with-cairo \
+  --with-libpng \
+  --with-jpeglib \
+  --with-x
+make
+make pdf
+make info
+make check
+make install
+make install-info
+make install-pdf
+cd ..
+
+# clean up
+rm -fr ${WHAT} ${DIR}
+
+/sbin/ldconfig
+/sbin/SuSEconfig
