eef9c62f9011873499d6d4d466489e17ec37eb2c
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-03-30 17:03:50 -0700

    scripts to replace R-patched with R-beta

---
+++ Desktop/Install-Scripts/R-beta/install-R.bash
@@ -0,0 +1,44 @@
+#! /bin/bash -v
+
+# first, get rid of installed R!
+zypper remove R-patched R-patched-devel
+
+mkdir -p /usr/local/src
+cd /usr/local/src
+export WHERE=http://cran.r-project.org/src/base-prerelease/
+export DIR=R-beta
+export WHAT=R-latest.tar.gz
+rm -fr ${WHAT} ${DIR}
+wget ${WHERE}/${WHAT}
+tar xf ${WHAT}
+
+cd ${DIR}
+export R_PAPERSIZE='letter'
+export R_BROWSER='chromium'
+export R_PDFVIEWER='evince'
+./configure \
+  --enable-threads \
+  --enable-static \
+  --enable-R-profiling \
+  --enable-BLAS-shlib \
+  --enable-R-shlib \
+  --enable-R-static-lib \
+  --with-tcltk \
+  --with-cairo \
+  --with-libpng \
+  --with-jpeglib \
+  --with-x
+make
+make pdf
+make info
+make check
+make install
+make install-info
+make install-pdf
+cd ..
+
+# clean up
+#rm -fr ${WHAT} ${DIR}
+
+/sbin/ldconfig
+/sbin/SuSEconfig
