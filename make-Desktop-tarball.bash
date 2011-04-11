410ec1a7ee7846a4b655d64cfb8f395ed2364e91
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-04-11 02:31:11 -0700

    add Compendium unpack

--- make-Desktop-tarball.bash
+++ make-Desktop-tarball.bash
@@ -5,6 +5,8 @@ wget http://google-refine.googlecode.com/files/google-refine-2.0-r1836.tar.gz
 pushd Desktop
 rm -fr google-refine-2.0
 tar xf ../google-refine-2.0-r1836.tar.gz
+rm -fr Compendium
+tar xf ../compendium-2.0-beta1.tar.gz
 popd
 pushd Desktop/Install-Scripts/
 ./cleanup.bash
