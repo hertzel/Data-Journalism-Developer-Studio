fe377f32f7c5a8b13c6e0b94c2e415671722cec1
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-04-18 07:07:42 -0700

    moved armadillo and quantlib from install-dependencies to install-all

--- Desktop/Install-Scripts/Finance/install-all.bash
+++ Desktop/Install-Scripts/Finance/install-all.bash
@@ -3,4 +3,11 @@
 # let X work
 export DISPLAY=:0.0
 xhost +
+
+# warning: quantlib takes a *long* time and a lot of space
+for i in quantlib armadillo
+do
+  sudo ./install-${i}.bash 2>&1 | tee install-${i}.log
+done
+
 sudo ./install-task-view.bash 2>&1 | tee install-task-view.log
--- Desktop/Install-Scripts/Finance/install-dependencies.bash
+++ Desktop/Install-Scripts/Finance/install-dependencies.bash
@@ -1,7 +1,3 @@
 #! /bin/bash -v
 
 sudo zypper install -y gsl gsl-devel
-for i in quantlib armadillo
-do
-  sudo ./install-${i}.bash 2>&1 | tee install-${i}.log
-done
