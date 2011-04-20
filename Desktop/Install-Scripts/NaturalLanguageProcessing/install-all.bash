97b79ec9cd00d88ee23d773256e29ad5b53ca822
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-04-20 15:00:27 -0700

    moved Wordnet into install-all.bash for consistency

--- Desktop/Install-Scripts/NaturalLanguageProcessing/install-all.bash
+++ Desktop/Install-Scripts/NaturalLanguageProcessing/install-all.bash
@@ -3,4 +3,7 @@
 # let X work
 export DISPLAY=:0.0
 xhost +
-sudo ./install-task-view.bash 2>&1 | tee install-task-view.log
+for i in wordnet task-view
+do
+  sudo ./install-${i}.bash 2>&1 | tee install-${i}.log
+done
--- Desktop/Install-Scripts/NaturalLanguageProcessing/install-dependencies.bash
+++ Desktop/Install-Scripts/NaturalLanguageProcessing/install-dependencies.bash
@@ -1,7 +1,3 @@
 #! /bin/bash -v
 
 sudo zypper install -y tcl-devel tk-devel
-for i in wordnet
-do
-  sudo ./install-${i}.bash 2>&1 | tee install-${i}.log
-done
