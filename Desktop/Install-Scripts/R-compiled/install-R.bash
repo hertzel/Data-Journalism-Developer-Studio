7679fdfeb151b0d730fcf02827d8d1395c557a22
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-04-20 08:15:27 -0700

    100% local!

--- Desktop/Install-Scripts/R-compiled/install-R.bash
+++ Desktop/Install-Scripts/R-compiled/install-R.bash
@@ -1,10 +1,10 @@
 #! /bin/bash -v
 
-mkdir -p /usr/local/src
-pushd /usr/local/src
-export WHERE=http://cran.cnr.berkeley.edu/src/base/R-2
+# make local source tree
+export SOURCE=~/src
+mkdir -p ${SOURCE}
+pushd ${SOURCE}
 export WHERE=ftp://ftp.stat.math.ethz.ch/Software/R
-export DIR=R-2.13.0
 export DIR=R-patched
 export WHAT=${DIR}.tar.gz
 rm -fr ${WHAT} ${DIR}
@@ -13,14 +13,19 @@ tar xf ${WHAT}
 rm -fr ${WHAT}
 popd
 
-export BUILD=/usr/local/R-compiled
+# make local build / install tree
+export BUILD=~/R-compiled
 rm -fr ${BUILD}
 mkdir -p ${BUILD}
 pushd ${BUILD}
+
+# environment variables
 export R_PAPERSIZE='letter'
 export R_BROWSER='/usr/bin/chromium'
 export R_PDFVIEWER='/usr/bin/evince'
-/usr/local/src/${DIR}/configure \
+
+# configure
+${SOURCE}/${DIR}/configure \
   --enable-threads \
   --enable-static \
   --enable-shared \
@@ -33,9 +38,13 @@ export R_PDFVIEWER='/usr/bin/evince'
   --with-libpng \
   --with-jpeglib \
   --with-x
+
+# compile with bytecode
 make bytecode 2>&1 | tee bytecode.log
 make pdf 2>&1 | tee pdf.log
-export PATH=/usr/local/R-compiled/bin:${PATH}
+
+# check
+export PATH=${BUILD}/bin:${PATH}
 export R_COMPILE_PKGS=1
 export R_ENABLE_JIT=0
 make check-all 2>&1 | tee check-all.log
--- Desktop/Install-Scripts/R-compiled/install-all.bash
+++ Desktop/Install-Scripts/R-compiled/install-all.bash
@@ -1,10 +1,11 @@
 #! /bin/bash -v
 
-# let X work
-export DISPLAY=:0.0
-xhost +
-
 for i in R system
 do
-  sudo ./install-${i}.bash 2>&1 | tee ${i}.log
+  ./install-${i}.bash 2>&1 | tee ${i}.log
 done
+
+# edit .bashrc
+vi ~/.bashrc
+echo 'export PATH=~/R-compiled/bin:${PATH}' >> ~/.bashrc
+echo 'export R_COMPILE_PKGS=1' >> ~/.bashrc
