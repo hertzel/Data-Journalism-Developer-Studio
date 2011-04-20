18fe67fa91612f25eb05e5fdc6bbf166bbcde609
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-04-20 09:39:37 -0700

    added local R to search

--- Desktop/Install-Scripts/Sweave-LyX/install.bash
+++ Desktop/Install-Scripts/Sweave-LyX/install.bash
@@ -7,7 +7,7 @@ cp test.lyx Sweave-test-1.lyx ~/.lyx/examples/
 
 # now install in TeX 
 sudo mkdir -p /usr/share/texmf/tex/latex/R
-for d in "local/lib" "local/lib64" "lib" "lib64"
+for d in "local/lib" "local/lib64" "lib" "lib64" "~/R-compiled/lib"
 do
   if [[ -e /usr/${d}/R/share/texmf ]]
   then
