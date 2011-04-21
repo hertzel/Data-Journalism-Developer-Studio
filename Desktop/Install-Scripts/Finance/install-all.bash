b5f19738352a3f05d70d2b53d928cfcf540311d6
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-04-21 08:10:33 -0700

    cleanup

--- Desktop/Install-Scripts/Finance/install-all.bash
+++ Desktop/Install-Scripts/Finance/install-all.bash
@@ -5,9 +5,7 @@ export DISPLAY=:0.0
 xhost +
 
 # warning: quantlib takes a *long* time and a lot of space
-for i in quantlib armadillo
+for i in quantlib armadillo task-view
 do
   sudo ./install-${i}.bash 2>&1 | tee install-${i}.log
 done
-
-sudo ./install-task-view.bash 2>&1 | tee install-task-view.log
