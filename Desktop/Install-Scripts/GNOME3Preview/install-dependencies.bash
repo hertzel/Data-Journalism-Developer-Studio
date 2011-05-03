1815021b53ac2ffb75a075a3699bc6688131660f
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-03 12:58:45 -0700

    added tracker desktop search utilities

--- Desktop/Install-Scripts/GNOME3Preview/install-dependencies.bash
+++ Desktop/Install-Scripts/GNOME3Preview/install-dependencies.bash
@@ -1,3 +1,6 @@
 #! /bin/bash -v
 
 sudo zypper install -y gnome-shell
+sudo zypper install -y tracker tracker-miner-evolution tracker-miner-files \
+  tracker-miner-flickr tracker-miner-rss tracker-gui \
+  gnome-panel-applet-tracker nautilus-extension-tracker-tags
