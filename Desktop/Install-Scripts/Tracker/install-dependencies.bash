f89a4595f01646fb5dde41016d1d3dc6ed7be252
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-03 15:32:52 -0700

    just do tracker for now - gnome-shell doesn't work in a VM

---
+++ Desktop/Install-Scripts/Tracker/install-dependencies.bash
@@ -0,0 +1,5 @@
+#! /bin/bash -v
+
+sudo zypper install -y tracker tracker-miner-evolution tracker-miner-files \
+  tracker-miner-flickr tracker-miner-rss tracker-gui \
+  gnome-panel-applet-tracker nautilus-extension-tracker-tags
