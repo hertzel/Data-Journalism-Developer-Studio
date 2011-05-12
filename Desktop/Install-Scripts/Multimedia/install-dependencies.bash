d7cd9c3b8df7c93e1fb182d7172c41d1f5082b30
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-12 11:49:26 -0700

    need freeglut-devel and libpng14-devel in main build

--- Desktop/Install-Scripts/Multimedia/install-dependencies.bash
+++ Desktop/Install-Scripts/Multimedia/install-dependencies.bash
@@ -1,8 +1,7 @@
 #! /bin/bash -v
 
 # needed for R audio  packages
-sudo zypper install -y fftw3 fftw3-devel fftw3-threads fftw3-threads-devel \
-  freeglut-devel libpng14-devel
+sudo zypper install -y fftw3 fftw3-devel fftw3-threads fftw3-threads-devel
 
 # base pattern
 sudo zypper install -y -t pattern multimedia
--- Desktop/Install-Scripts/install-dependencies.bash
+++ Desktop/Install-Scripts/install-dependencies.bash
@@ -35,3 +35,4 @@ sudo zypper update
 sudo zypper --non-interactive in chromium vpx-tools
 sudo zypper --non-interactive in R-patched R-patched-devel
 sudo zypper --non-interactive in tesseract
+sudo zypper --non-interactive in freeglut-devel libpng14-devel
