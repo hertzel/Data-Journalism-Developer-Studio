1558b1b3751c0aa98ff6936837c064bf73591b68
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-11 20:33:45 -0700

    added config files to archives

--- Desktop/Install-Scripts/make-archives.bash
+++ Desktop/Install-Scripts/make-archives.bash
@@ -1,5 +1,8 @@
 #! /bin/bash -v
 cd ~
+tar cvf - /etc/shadow /etc/passwd /etc/sysconfig/SuSEfirewall2 \
+  /etc/init.d/rstudio-server /etc/init.d/.depend.halt /etc/init.d/.depend.stop \
+  /etc/init.d/.depend.boot /etc/init.d/.depend.start | bzip2 -9c > etc.tar.bz2
 tar cvf - /usr/lib/R | bzip2 -9c > usr-lib-R.tar.bz2
 tar cvf - /etc/init.d/rstudio-server /etc/pam.d/rstudio /usr/local \
   | bzip2 -9c > usr-local.tar.bz2
