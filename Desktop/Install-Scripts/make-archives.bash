296cecabb97e2433c916ec7deb4fd497d0bcad12
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-12 08:23:24 -0700

    added missing files

--- Desktop/Install-Scripts/make-archives.bash
+++ Desktop/Install-Scripts/make-archives.bash
@@ -1,9 +1,15 @@
 #! /bin/bash -v
 cd ~
-tar cvf - /etc/shadow /etc/passwd /etc/sysconfig/SuSEfirewall2 \
-  /etc/init.d/rstudio-server /etc/init.d/.depend.halt /etc/init.d/.depend.stop \
-  /etc/init.d/.depend.boot /etc/init.d/.depend.start | bzip2 -9c > etc.tar.bz2
+tar cvf - \
+  /etc/shadow /etc/passwd \
+  /usr/sbin/rstudio-server \
+  /etc/init.d/rstudio-server \
+  /sbin/rcrstudio-server \
+  /etc/pam.d/rstudio \
+  /etc/sysconfig/SuSEfirewall2 \
+  /etc/init.d/.depend.halt /etc/init.d/.depend.stop \
+  /etc/init.d/.depend.boot /etc/init.d/.depend.start \
+  | bzip2 -9c > config.tar.bz2
 tar cvf - /usr/lib/R | bzip2 -9c > usr-lib-R.tar.bz2
-tar cvf - /etc/init.d/rstudio-server /etc/pam.d/rstudio /usr/local \
-  | bzip2 -9c > usr-local.tar.bz2
+tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
 tar cvf - .bashrc perl5 .cpan | bzip2 -9c > perl5.tar.bz2
