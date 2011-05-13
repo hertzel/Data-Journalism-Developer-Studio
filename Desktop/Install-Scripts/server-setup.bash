55b38a04172f54976aef0e1ad974a22ce6e4383c
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-13 09:09:58 -0700

    use RStudio pam and init scripts now

--- Desktop/Install-Scripts/server-setup.bash
+++ Desktop/Install-Scripts/server-setup.bash
@@ -4,12 +4,14 @@
 sudo /usr/sbin/useradd -r rstudio-server
 
 # soft link
-sudo ln -sf /usr/local/rstudio-server/bin/rstudio-server /usr/sbin/rstudio-server
+sudo ln -sf /usr/local/rstudio-server/bin/rstudio-server \
+  /usr/sbin/rstudio-server
 
 # config files
-sudo cp initscript /etc/init.d/rstudio-server
-sudo ln -s /etc/init.d/rstudio-server /sbin/rcrstudio-server
-sudo cp pamscript /etc/pam.d/rstudio
+sudo cp /usr/local/lib/rstudio-server/extras/init.d/suse/rstudio-server \
+  /etc/init.d/rstudio-server
+sudo ln -sf /etc/init.d/rstudio-server /sbin/rcrstudio-server
+sudo cp /usr/local/lib/rstudio-server/extras/pam/rstudio /etc/pam.d/rstudio
 
 # firewall - open port 8787
 sudo /sbin/yast2 firewall
