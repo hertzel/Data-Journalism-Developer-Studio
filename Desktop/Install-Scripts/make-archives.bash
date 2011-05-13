f741f8b97035f7b7f5600bb77adaeffedbd7c3f0
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-13 09:13:38 -0700

    slight refactor

--- Desktop/Install-Scripts/make-archives.bash
+++ Desktop/Install-Scripts/make-archives.bash
@@ -4,9 +4,9 @@ tar cvf - /etc/sysconfig/SuSEfirewall2 | bzip2 -9c > firewall.tar.bz2
 tar cvf - /etc/inittab /etc/init.d/.depend.halt /etc/init.d/.depend.stop \
   /etc/init.d/.depend.boot /etc/init.d/.depend.start \
   /etc/rstudio/secure-cookie-key | bzip2 -9c > runlevels.tar.bz2
-tar cvf - /etc/shadow /etc/passwd /usr/sbin/rstudio-server \
-  /etc/init.d/rstudio-server /sbin/rcrstudio-server /etc/pam.d/rstudio \
-  | bzip2 -9c > rstudio.tar.bz2
+tar cvf - /etc/shadow /etc/passwd | bzip2 -9c > useradd.tar.bz2
+tar cvf - /usr/sbin/rstudio-server /etc/init.d/rstudio-server \
+  /sbin/rcrstudio-server /etc/pam.d/rstudio | bzip2 -9c > rstudio.tar.bz2
 tar cvf - /usr/lib/R | bzip2 -9c > usr-lib-R.tar.bz2
 tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
 tar cvf - .bashrc perl5 .cpan | bzip2 -9c > perl5.tar.bz2
