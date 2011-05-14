881ca0e94849e1be42d5d2519d8c026dec4cb542
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-13 17:17:59 -0700

    Django

---
+++ Desktop/Install-Scripts/Django/install-dependencies.bash
@@ -0,0 +1,8 @@
+#! /bin/bash -v
+
+# Repository
+sudo zypper addrepo -cf http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_11.4/devel:languages:python.repo
+sudo zypper refresh
+sudo zypper update
+
+sudo zypper install -y python-django
