3229a7e4de0b8728bf5d7f38cd35afdf196fffbe
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-15 18:52:02 -0700

    ditched AlgoCompSynth - added these packages to Multimedia

--- Desktop/Install-Scripts/AlgoCompSynth/install-dependencies.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -v
-
-sudo zypper addrepo -cf \
-  http://download.opensuse.org/repositories/multimedia:/apps/openSUSE_11.4/multimedia:apps.repo
-sudo zypper refresh
-sudo zypper update
-
-sudo zypper install -y rosegarden lilypond qjackctl csound cecilia hydrogen \
-  ZynAddSubFX fluidsynth-dssi xsynth-dssi fluidsynth
