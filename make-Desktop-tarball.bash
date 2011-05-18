c27fb52feae34ab0fcff07526376faceeb13ce59
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-18 12:45:35 -0700

    use cleanup.bash instead of explicit removes

--- make-Desktop-tarball.bash
+++ make-Desktop-tarball.bash
@@ -1,12 +1,7 @@
 #! /bin/bash -v
-rm -f Desktop.tar.bz2
-rm -f google-refine*.tar.gz
+./cleanup.bash
 wget http://google-refine.googlecode.com/files/google-refine-2.0-r1836.tar.gz
 pushd Desktop
-rm -fr google-refine-2.0
 tar xf ../google-refine-2.0-r1836.tar.gz
-popd
-pushd Desktop/Install-Scripts/
-./cleanup.bash
 popd
 tar cvf - Desktop | bzip2 -9c > Desktop.tar.bz2
