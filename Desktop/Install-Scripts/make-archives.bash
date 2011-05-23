ebd84c7d8a4fe3433c9d552d68430c228c9f04c1
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-23 00:10:14 -0700

    run all with root privileges

--- Desktop/Install-Scripts/make-archives.bash
+++ Desktop/Install-Scripts/make-archives.bash
@@ -1,9 +1,8 @@
 #! /bin/bash -v
 cd ~
-tar cvf - /etc/sysconfig/SuSEfirewall2 | bzip2 -9c > firewall.tar.bz2
+sudo tar cvf - /etc/sysconfig/SuSEfirewall2 | bzip2 -9c > firewall.tar.bz2
 sudo tar cvf - /etc/shadow /etc/passwd | bzip2 -9c > useradd.tar.bz2
-tar cvf - /etc/sysconfig/SuSEfirewall2 | bzip2 -9c > firewall.tar.bz2
-tar cvf - /usr/sbin/rstudio-server /etc/init.d/rstudio-server \
+sudo tar cvf - /usr/sbin/rstudio-server /etc/init.d/rstudio-server \
   /sbin/rcrstudio-server /etc/pam.d/rstudio | bzip2 -9c > rstudio.tar.bz2
-tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
-tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
+sudo tar cvf - /usr/local | bzip2 -9c > usr-local.tar.bz2
+sudo tar cvf - .bashrc | bzip2 -9c > bashrc.tar.bz2
