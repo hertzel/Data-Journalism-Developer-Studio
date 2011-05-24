8d5b88e34829c36a6b09720b4be3d8441ea2e3a4
M. Edward (Ed) Borasky <znmeb@AlgoCompSynth.(none)>
2011-05-24 16:00:17 -0700

    just install local::lib from openSUSE Perl repo

--- Desktop/Install-Scripts/Perl-packages/install-all.bash
+++ Desktop/Install-Scripts/Perl-packages/install-all.bash
@@ -10,20 +10,37 @@ cpan < yesyes.cpan # first, autoconfigure
 cpan < prep.cpan # set config options
 
 for i in \
+  AnyEvent::Twitter::Stream
+  Browser::Open \
+  Data::Dumper \
+  DBD::SQLite \
+  Email::Send \
   Email::Send::Gmail \
   Email::Sender::Simple \
+  Email::Simple \
   Email::Simple::Creator \
   Facebook \
   Google::Data::JSON \
   Google::Voice \
   Hash::Flatten \
   HTTP::Client::Parallel \
+  HTTP::Date \
+  JSON::XS \
   MIME::Base64 \
+  Module::Build \
   Net::Google::Spreadsheets \
+  Net::Twitter \
+  POE \
+  Software::License \
   Statistics::R \
   Statistics::R::Bridge::Linux \
+  Try::Tiny \
   URI::Escape \
-  WebService::Bitly
+  UNIVERSAL::require \
+  WebService::Bitly \
+  WWW::Mechanize \
+  XML::XPath \
+  YAML::Tiny
 do
   echo "notest install $i" | sudo cpan 2>&1 | tee $i.log
 done
--- Desktop/Install-Scripts/Perl-packages/install-dependencies.bash
+++ Desktop/Install-Scripts/Perl-packages/install-dependencies.bash
@@ -4,21 +4,7 @@ sudo zypper addrepo -cf \
   http://download.opensuse.org/repositories/devel:/languages:/perl/openSUSE_11.4/devel:languages:perl.repo
 sudo zypper refresh
 sudo zypper update
-sudo zypper dup --from=devel_languages_perl
 
-sudo zypper install -y perl-AnyEvent-Twitter-Stream
-sudo zypper install -y perl-Browser-Open
-sudo zypper install -y perl-Data-Dumper
-sudo zypper install -y perl-DBD-SQLite
-sudo zypper install -y perl-Email-Send
-sudo zypper install -y perl-Email-Simple
-sudo zypper install -y perl-HTTP-Date
-sudo zypper install -y perl-JSON-XS
 sudo zypper install -y perl-local-lib
-sudo zypper install -y perl-Net-Twitter
-sudo zypper install -y perl-POE
-sudo zypper install -y perl-Software-License
-sudo zypper install -y perl-WWW-Mechanize
 sudo zypper install -y perl-YAML
 sudo zypper install -y perl-YAML-Syck
-sudo zypper install -y perl-YAML-Tiny
