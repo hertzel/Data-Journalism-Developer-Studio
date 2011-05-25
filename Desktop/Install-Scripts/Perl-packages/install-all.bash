417f2b1693e32d90953f61a790a4b8b724c952ee
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-25 00:26:41 -0700

    factored out package install

--- Desktop/Install-Scripts/Perl-packages/install-all.bash
+++ Desktop/Install-Scripts/Perl-packages/install-all.bash
@@ -1,31 +1,6 @@
 #! /bin/bash -v
 
-./install-local-lib.bash
-
-. ~/.bashrc # switch to local Perl
-
-for i in \
-  AnyEvent::Twitter::Stream \
-  Browser::Open \
-  DBD::SQLite \
-  Facebook \
-  Hash::Flatten \
-  JSON::XS \
-  Net::Twitter \
-  Statistics::R \
-  Statistics::R::Bridge::Linux \
-  YAML::Syck \
-  Data::Dumper \
-  Email::Send::Gmail \
-  Email::Send \
-  Email::Sender::Simple \
-  Email::Simple::Creator \
-  Email::Simple \
-  HTTP::Client::Parallel \
-  Net::Google::Spreadsheets \
-  Try::Tiny \
-  WebService::Bitly \
-  Google::Voice
+for i in local-lib packages
 do
-  echo "notest install $i" | cpan 2>&1 | tee $i.log
+  ./install-${i}.bash 2>&1 | tee ${i}.log
 done
---
+++ Desktop/Install-Scripts/Perl-packages/install-packages.bash
@@ -0,0 +1,28 @@
+#! /bin/bash -v
+
+for i in \
+  AnyEvent::Twitter::Stream \
+  Browser::Open \
+  DBD::SQLite \
+  Facebook \
+  Hash::Flatten \
+  JSON::XS \
+  Net::Twitter \
+  Statistics::R \
+  Statistics::R::Bridge::Linux \
+  WWW::Mechanize \
+  YAML::Syck \
+  Data::Dumper \
+  Email::Send::Gmail \
+  Email::Send \
+  Email::Sender::Simple \
+  Email::Simple::Creator \
+  Email::Simple \
+  HTTP::Client::Parallel \
+  Net::Google::Spreadsheets \
+  Try::Tiny \
+  WebService::Bitly \
+  Google::Voice
+do
+  echo "notest install $i" | cpan 2>&1 | tee $i.log
+done
