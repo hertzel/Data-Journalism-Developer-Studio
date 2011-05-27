508035057a4c729905a8d4bb85e480e73982a6a8
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-27 02:16:13 -0700

    added MS Word HTML export

--- Desktop/Install-Scripts/Sweave-LyX/install-dependencies.bash
+++ Desktop/Install-Scripts/Sweave-LyX/install-dependencies.bash
@@ -1,3 +1,9 @@
 #! /bin/bash -v
 
-sudo zypper install -y lyx
+sudo zypper addrepo -cf \
+  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
+sudo zypper refresh
+sudo zypper update
+
+sudo zypper install -y lyx latex2html latex2html-doc latex2html-pngicons \
+  texlive-tex4ht
