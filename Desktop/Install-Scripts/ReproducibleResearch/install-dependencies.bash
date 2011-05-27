3844f4c2986cbea280db96ac2858bdfae891e0e9
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-27 13:40:43 -0700

    add Publishing repo here too

--- Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -1,4 +1,9 @@
 #! /bin/bash -v
 
+sudo zypper addrepo -cf \
+  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
+sudo zypper refresh
+sudo zypper update
+
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
 sudo zypper install -y texinfo texlive texlive-latex texlive-xetex
