3371105079df5a357c4dcce2cd257712c0cca345
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-27 13:42:06 -0700

    only install PostgreSQL and friends once!

--- Desktop/Install-Scripts/common/install-postgresql.bash
+++ Desktop/Install-Scripts/common/install-postgresql.bash
@@ -1,12 +1,19 @@
 #! /bin/bash -v
 
-sudo zypper addrepo -cf \
+TEST=`/sbin/chkconfig | grep postgresql | wc -l`
+if [ "$TEST" != "1" ]
+then
+  echo "Installing PostgreSQL and friends"
+  sudo zypper addrepo -cf \
   http://download.opensuse.org/repositories/server:/database:/postgresql/openSUSE_11.4/server:database:postgresql.repo
-sudo zypper refresh
-sudo zypper update
+  sudo zypper refresh
+  sudo zypper update
 
-sudo zypper install -y postgresql postgresql-devel postgresql-contrib \
-  postgresql-docs pgadmin3 perl-DBD-Pg
+  sudo zypper install -y postgresql postgresql-devel postgresql-contrib \
+    postgresql-docs pgadmin3 perl-DBD-Pg
 
-sudo /sbin/chkconfig postgresql on
-sudo /etc/init.d/postgresql restart
+  sudo /sbin/chkconfig postgresql on
+  sudo /etc/init.d/postgresql start
+else
+  echo "PostgreSQL and friends have already been installed"
+fi
