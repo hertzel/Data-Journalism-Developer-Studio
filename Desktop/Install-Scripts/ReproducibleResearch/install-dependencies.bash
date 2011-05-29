38bb4587600bad727c794d58f7ebd3adf31077da
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-28 23:49:09 -0700

    ditched "Publishing" repository - causes conflicts

--- Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -1,9 +1,4 @@
 #! /bin/bash -v
 
-sudo zypper addrepo -cf \
-  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
-sudo zypper refresh
-sudo zypper update
-
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
-sudo zypper install -y texinfo texlive texlive-latex texlive-xetex
+sudo zypper install -y texlive texlive-latex texlive-xetex
--- Desktop/Install-Scripts/Sweave-LyX/install-dependencies.bash
+++ Desktop/Install-Scripts/Sweave-LyX/install-dependencies.bash
@@ -1,9 +1,4 @@
 #! /bin/bash -v
 
-sudo zypper addrepo -cf \
-  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
-sudo zypper refresh
-sudo zypper update
-
 sudo zypper install -y lyx latex2html latex2html-doc latex2html-pngicons \
   texlive-tex4ht
