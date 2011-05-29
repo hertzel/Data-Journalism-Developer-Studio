552dad4bbd3d07088a4064ac6a0000de0e6d8343
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-29 00:33:14 -0700

    chromium-devel doesn't exist any more??

--- Desktop/Install-Scripts/BrowserDevelopment/install-dependencies.bash
+++ Desktop/Install-Scripts/BrowserDevelopment/install-dependencies.bash
@@ -6,5 +6,5 @@ http://download.opensuse.org/repositories/openSUSE:/11.4:/Contrib/standard/openS
 sudo zypper refresh
 sudo zypper update
 
-sudo zypper install -y chromium vpx-tools chromium-devel v8-devel libvpx-devel
+sudo zypper install -y chromium vpx-tools v8-devel libvpx-devel
 sudo zypper install -y MozillaFirefox-devel
