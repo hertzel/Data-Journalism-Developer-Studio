0eeca8e8fc6bdd7b78422132563793ec90d13e7c
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-05-29 00:36:18 -0700

    we don't need the repository - it's in the base appliance

--- Desktop/Install-Scripts/BrowserDevelopment/install-dependencies.bash
+++ Desktop/Install-Scripts/BrowserDevelopment/install-dependencies.bash
@@ -1,10 +1,4 @@
 #! /bin/bash -v
 
-# Chromium lives here
-sudo zypper addrepo -cf \
-http://download.opensuse.org/repositories/openSUSE:/11.4:/Contrib/standard/openSUSE:11.4:Contrib.repo
-sudo zypper refresh
-sudo zypper update
-
 sudo zypper install -y chromium vpx-tools v8-devel libvpx-devel
 sudo zypper install -y MozillaFirefox-devel
