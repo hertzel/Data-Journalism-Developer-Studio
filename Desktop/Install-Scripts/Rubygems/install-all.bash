e9af2339f60e200ce1130a1985b6337ebc4b3ed0
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-06-18 19:09:52 -0700

    fixed bug when R is in /usr/local

--- Desktop/Install-Scripts/Rubygems/install-all.bash
+++ Desktop/Install-Scripts/Rubygems/install-all.bash
@@ -9,3 +9,11 @@ if [ -e /usr/lib64/R ]
 then
   sudo gem install rsruby -- --with-R-dir=/usr/lib64/R
 fi
+if [ -e /usr/local/lib/R ]
+then
+  sudo gem install rsruby -- --with-R-dir=/usr/local/lib/R
+fi
+if [ -e /usr/local/lib64/R ]
+then
+  sudo gem install rsruby -- --with-R-dir=/usr/local/lib64/R
+fi
