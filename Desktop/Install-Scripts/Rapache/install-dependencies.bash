757c01cafcf730a6c1d0a1fb7400e2fbf2fe09fe
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-06-26 10:48:29 -0700

    added awstats to dependencies

--- Desktop/Install-Scripts/Rapache/install-dependencies.bash
+++ Desktop/Install-Scripts/Rapache/install-dependencies.bash
@@ -1,4 +1,8 @@
 #! /bin/bash -v
 
+sudo zypper addrepo -cf \
+http://download.opensuse.org/repositories/network:/utilities/openSUSE_11.4/network:utilities.repo
+sudo zypper update
+
 sudo zypper install -y apache2 apache2-devel apache2-doc apache2-prefork \
-  apache2-utils yast2-http-server
+  apache2-utils yast2-http-server awstats
