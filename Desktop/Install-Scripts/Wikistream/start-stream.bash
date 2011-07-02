194fb6f672568aba966603f84c6528f4c51e2a99
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-07-01 18:02:58 -0700

    open browser!

--- Desktop/Install-Scripts/Wikistream/start-stream.bash
+++ Desktop/Install-Scripts/Wikistream/start-stream.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 
-sudo /etc/init.d/redis start
+sudo /etc/init.d/redis restart
 nohup node /opt/wikistream/updates.js > updates.nohup &
 nohup node /opt/wikistream/app.js > app.nohup &
-echo "Browse to localhost:3000"
+firefox localhost:3000 &
