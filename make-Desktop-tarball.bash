4f9f1f444f189a4d654189aad76f189dc04edc96
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-07-18 18:10:37 -0700

    Google Refine update to 2.1

--- make-Desktop-tarball.bash
+++ make-Desktop-tarball.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -v
 ./cleanup.bash
-wget http://google-refine.googlecode.com/files/google-refine-2.0-r1836.tar.gz
+wget http://google-refine.googlecode.com/files/google-refine-2.1-r2136.tar.gz
 pushd Desktop
 tar xf ../google-refine-2.0-r1836.tar.gz
 popd
