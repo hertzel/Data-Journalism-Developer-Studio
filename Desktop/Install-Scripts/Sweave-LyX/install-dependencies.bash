90908876c140864a7c6a67078d557c2d1cc36fb0
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-08-16 11:51:52 -0700

    switching to Publishing repo and ditching upgrade script

--- Desktop/Install-Scripts/Sweave-LyX/install-dependencies.bash
+++ Desktop/Install-Scripts/Sweave-LyX/install-dependencies.bash
@@ -1,4 +1,9 @@
 #! /bin/bash -v
 
+# get latest LaTeX and LyX
+sudo zypper addrepo -cf \
+  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
+sudo zypper update
+
 sudo zypper install -y lyx latex2html latex2html-doc latex2html-pngicons \
   texlive-tex4ht
--- Desktop/Install-Scripts/Sweave-LyX/upgrade-lyx.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -v
-
-sudo zypper addrepo -cf \
-  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
-sudo zypper refresh
-sudo zypper update
-
-sudo zypper remove -y lyx
-sudo zypper install lyx
