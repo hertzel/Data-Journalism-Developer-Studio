5378366de636b57689593a80a87880eddc572177
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-08-16 11:52:45 -0700

    use Publishing repo to get latest LaTeX tools

--- Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -1,4 +1,10 @@
 #! /bin/bash -v
 
+# get most recent LaTex tools
+sudo zypper addrepo -cf \
+  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
+sudo zypper update
+
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
 sudo zypper install -y texlive texlive-latex texlive-xetex
+
