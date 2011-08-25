b6534d4e23b6c20fa088291de5101355a9261f2b
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-08-24 22:01:39 -0700

    moved document creation to Reproducible Research

--- Desktop/Install-Scripts/Multimedia/install-dependencies.bash
+++ Desktop/Install-Scripts/Multimedia/install-dependencies.bash
@@ -20,26 +20,6 @@ sudo zypper install -y blender blender-doc
 sudo zypper install -y pitivi
 sudo zypper install -y gtk-recordMyDesktop
 
-# document creation
-sudo zypper install -y scribus
-sudo zypper install -y inkscape
-sudo zypper install -y inkscape-extensions-dia
-sudo zypper install -y inkscape-extensions-extra
-sudo zypper install -y inkscape-extensions-fig
-sudo zypper install -y inkscape-extensions-gimp
-sudo zypper install -y inkscape-extensions-skencil
-sudo zypper install -y gimp
-sudo zypper install -y gimp-branding-openSUSE
-sudo zypper install -y gimp-gap
-sudo zypper install -y gimp-help
-sudo zypper install -y gimp-module-hal
-sudo zypper install -y gimp-plugin-lqr
-sudo zypper install -y gimp-plugins-python
-sudo zypper install -y gimp-save-for-web
-sudo zypper install -y gimp-ufraw ufraw
-sudo zypper install -y gutenprint-gimpplugin
-sudo zypper install -y calibre
-
 # Algorithmic composition and synthesis
 sudo zypper install -y rosegarden lilypond qjackctl csound cecilia hydrogen \
   ZynAddSubFX fluidsynth-dssi xsynth-dssi fluidsynth jamin noteedit \
