c5d4797afe924e6ae2250f9013302d68e998a4c7
M. Edward (Ed) Borasky <znmeb@borasky-research.net>
2011-08-24 22:00:22 -0700

    moved document creation here from Multimedia

--- Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
+++ Desktop/Install-Scripts/ReproducibleResearch/install-dependencies.bash
@@ -1,16 +1,28 @@
 #! /bin/bash -v
 
-# get most recent LaTex tools
-sudo zypper addrepo -cf \
-  http://download.opensuse.org/repositories/Publishing/openSUSE_11.4/Publishing.repo
-sudo zypper update
-
 # sigh - need the *whole* texlive-latex for pgfSweave :-(
 sudo zypper install -y texlive texlive-latex texlive-xetex
 
-<<<<<<< HEAD
 # For Sweave / LyX
 sudo zypper install -y lyx latex2html latex2html-doc latex2html-pngicons \
   texlive-tex4ht
-=======
->>>>>>> 5378366de636b57689593a80a87880eddc572177
+
+# document creation
+sudo zypper install -y scribus
+sudo zypper install -y inkscape
+sudo zypper install -y inkscape-extensions-dia
+sudo zypper install -y inkscape-extensions-extra
+sudo zypper install -y inkscape-extensions-fig
+sudo zypper install -y inkscape-extensions-gimp
+sudo zypper install -y inkscape-extensions-skencil
+sudo zypper install -y gimp
+sudo zypper install -y gimp-branding-openSUSE
+sudo zypper install -y gimp-gap
+sudo zypper install -y gimp-help
+sudo zypper install -y gimp-module-hal
+sudo zypper install -y gimp-plugin-lqr
+sudo zypper install -y gimp-plugins-python
+sudo zypper install -y gimp-save-for-web
+sudo zypper install -y gimp-ufraw ufraw
+sudo zypper install -y gutenprint-gimpplugin
+sudo zypper install -y calibre
